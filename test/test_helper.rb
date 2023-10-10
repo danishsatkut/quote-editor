efe7db4e3be47d7467e111f9809851e50d7d6437
Danish Aziz Satkut <donspy@gmail.com>
2023-10-10 09:58:57 +0530

    Fix simplecov due to rails parallelization (#5)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,28 @@
 # frozen_string_literal: true
 
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
-
 if ENV['COVERAGE']
   require 'simplecov'
   SimpleCov.start 'rails'
 end
 
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
+
+    if ENV['COVERAGE']
+      parallelize_setup do |worker|
+        SimpleCov.command_name "#{SimpleCov.command_name}-#{worker}"
+      end
+
+      parallelize_teardown do
+        SimpleCov.result
+      end
+    end
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
